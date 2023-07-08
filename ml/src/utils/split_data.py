c60f1703d8f6e4482a5007ed30969b36e868f873
JR-Camacho <josecamachoc0303@gmail.com>
2023-07-08 01:15:52 -0400

    New models are added for multiclass malware detection on android devices, specifically using 'decision trees' and 'random forest' techniques.

--- ml/src/utils/split_data.py
+++ ml/src/utils/split_data.py
@@ -8,3 +8,8 @@ def train_val_test_split(df, rstate=42, shuffle=True, stratify=None):
     val_set, test_set = train_test_split(
         test_set, test_size=0.5, random_state=rstate, shuffle=shuffle, stratify=strat)
     return (train_set, val_set, test_set)
+
+def remove_labels(df, label_name):
+    X = df.drop(label_name, axis=1)
+    y = df[label_name].copy()
+    return (X, y)
